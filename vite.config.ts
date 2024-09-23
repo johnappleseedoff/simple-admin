03ac2938181a838cecbef178b5121d4bec47e9eb
John Appleseedoff <john.appleseedoff@gmail.com>
2024-09-23 16:39:55 +0400

    server updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    //port: 4000,
-    origin: 'http://0.0.0.0:4000',
+    host: '0.0.0.0',
+    port: 4000,
+    //origin: 'http://0.0.0.0:4000',
   }
 })
